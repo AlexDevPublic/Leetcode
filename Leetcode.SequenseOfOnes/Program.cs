e980b93de8b4f604850d41479e70da035343fd2f
Alexandr Rakhmankulov <alexandr.rakhmankulov@smartcomds.ru>
2026-02-01 10:17:35 +0300

    init

---
+++ Leetcode.SequenseOfOnes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
+
+using var stream = new FileStream("input.txt", FileMode.Open);
+using var streamReader = new StreamReader(stream);
+
+var countOfNumbers = int.Parse(streamReader.ReadLine());
+
+var sequense = new int [countOfNumbers];
+var currentLine = 0;
+do
+{
+     sequense[currentLine] = int.Parse(streamReader.ReadLine());
+     currentLine++;
+} while (!streamReader.EndOfStream);
+
+var maxSequenseCount = 0;
+var currentSequenseMatches = 0;
+for (var i = 0; i < sequense.Length; i++)
+{
+    if (sequense[i] == 1)
+    {
+        currentSequenseMatches++;
+    }
+    
+    else
+    {
+        currentSequenseMatches = 0;
+    }
+
+    if (currentSequenseMatches > maxSequenseCount)
+    {
+        maxSequenseCount = currentSequenseMatches;
+    }
+}
+using var outputStream = new FileStream("output.txt", FileMode.OpenOrCreate);
+using var outputStreamWriter = new StreamWriter(outputStream);
+outputStreamWriter.WriteLine(maxSequenseCount);
